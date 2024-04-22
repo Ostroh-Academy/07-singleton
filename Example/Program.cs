821ff7e2afbcebbff3770f673c1e739bfd05faaa
Maxym_Tomusiak <101803422+Maksym-Tomusiak@users.noreply.github.com>
2024-04-22 13:21:11 +0300

    feat: add example from internet

---
+++ Example/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Singleton.Example
+{
+    public sealed class Logger
+    {
+        private static Logger instance = null;
+        private static readonly object padlock = new object();
+
+        private Logger()
+        {
+        }
+
+        public static Logger Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Logger();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"[{DateTime.Now}] {message}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = Logger.Instance;
+
+            logger.Log("This is a log message.");
+            logger.Log("Another log message.");
+        }
+    }
+}
