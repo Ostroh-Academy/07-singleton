229f816dbaa3a48f4f2ed45f4abe277af0f7998a
Nastya <92024271+havrylchyk@users.noreply.github.com>
2024-04-22 21:38:35 +0300

    Interner example

---
+++ Lab_7_Singleton_patterns/Lab_7_Singleton_patterns/Program.cs
@@ -0,0 +1,9 @@
+﻿Database foo = Database.GetInstance();
+foo.Query("SELECT ...");
+
+Database bar = Database.GetInstance();
+bar.Query("SELECT ...");
+
+// Обидва об'єкти foo і bar посилаються на один і той самий екземпляр бази даних.
+// Тобто, foo і bar рівні за посиланням і вказують на один об'єкт Database.
+Console.WriteLine(foo == bar); 
