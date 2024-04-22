1428bb47352e5770b2561a9ce479a8122aa1fc2d
Valeriia <valeriia.chyrko@oa.edu.ua>
2024-04-22 15:07:36 +0300

    Complete task.

---
+++ SingletonPatternAuth/Entities/AuthParams.cs
@@ -0,0 +1,17 @@
+﻿using SingletonPatternAuth.Entities.Interfaces;
+
+namespace SingletonPatternAuth.Entities;
+
+public class AuthParams : IAuthParams
+{
+    public string Username { get; }
+    public string EncryptedPassword { get; }
+    public DateTime Expiration { get; }
+
+    public AuthParams(string username, string encryptedPassword, DateTime expiration)
+    {
+        Username = username;
+        EncryptedPassword = encryptedPassword;
+        Expiration = expiration;
+    }
+}
---
+++ SingletonPatternAuth/Entities/AuthParamsBuilder.cs
@@ -0,0 +1,50 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using SingletonPatternAuth.Entities.Interfaces;
+
+namespace SingletonPatternAuth.Entities;
+
+public class AuthParamsBuilder : IAuthParamsBuilder
+{
+    private string _username = string.Empty;
+    private string _password = string.Empty;
+    private DateTime _expiration;
+
+    public IAuthParamsBuilder WithUsername(string username)
+    {
+        _username = username;
+        return this;
+    }
+
+    public IAuthParamsBuilder WithPassword(string password)
+    {
+        _password = password;
+        return this;
+    }
+
+    public IAuthParamsBuilder WithExpiration(DateTime expiration)
+    {
+        _expiration = expiration;
+        return this;
+    }
+
+    public IAuthParams Build()
+    {
+        if (string.IsNullOrEmpty(_username))
+            throw new InvalidOperationException("Username is required");
+        if (string.IsNullOrEmpty(_password))
+            throw new InvalidOperationException("Password is required");
+        if (_expiration == default(DateTime))
+            throw new InvalidOperationException("Expiration is required");
+
+        var encryptedPassword = Encrypt(_password);
+        return new AuthParams(_username, encryptedPassword, _expiration);
+    }
+
+    private string Encrypt(string input)
+    {
+        var inputBytes = Encoding.UTF8.GetBytes(input);
+        var hashBytes = MD5.HashData(inputBytes);
+        return Convert.ToBase64String(hashBytes);
+    }
+}
