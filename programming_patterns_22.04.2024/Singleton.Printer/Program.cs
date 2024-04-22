0f03149c9db0a0fb0185faa46cd1007c7f8c9bf0
Viktoriia Babaieva <viktoriia.babaieva@oa.edu.ua>
2024-04-22 13:33:20 +0300

    example and individual task added

---
+++ programming_patterns_22.04.2024/Singleton.Printer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Singleton.Printer
+{
+    public sealed class PDFPrinter
+    {
+        private PDFPrinter() { }
+
+        private static PDFPrinter _instance;
+
+        public static PDFPrinter GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new PDFPrinter();
+            }
+            return _instance;
+        }
+
+        public void PrintPDF(string source)
+        {
+            Console.WriteLine("Друк PDF з джерела: " + source);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PDFPrinter pdfPrinter = PDFPrinter.GetInstance();
+
+            pdfPrinter.PrintPDF("Лекційний матеріал");
+            pdfPrinter.PrintPDF("Наукова стаття");
+            pdfPrinter.PrintPDF("Дипломна робота");
+
+        }
+    }
+}
