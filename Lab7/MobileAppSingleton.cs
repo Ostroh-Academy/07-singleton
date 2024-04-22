7832fe001f6d8ee07f8810b6f7228b48b832863a
Marian-Zharchynskyi <145098603+Marian-Zharchynskyi@users.noreply.github.com>
2024-04-22 14:44:47 +0300

    Lab7 Example was added

---
+++ Lab7/MobileAppSingleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    public class MobileAppSingleton
+    {
+        private static MobileAppSingleton instance;
+
+        public string SomeData { get; set; }
+
+        private MobileAppSingleton()
+        {
+            SomeData = "Initial data";
+        }
+
+        public static MobileAppSingleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new MobileAppSingleton();
+            }
+            return instance;
+        }
+    }
+}
---
+++ Lab7/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Lab7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MobileAppSingleton instance1 = MobileAppSingleton.GetInstance();
+            Console.WriteLine(instance1.SomeData); 
+
+            instance1.SomeData = "Modified data";
+
+            MobileAppSingleton instance2 = MobileAppSingleton.GetInstance();
+            Console.WriteLine(instance2.SomeData); 
+
+            if (instance1 == instance2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Different instances of Singleton are returned.");
+            }
+        }
+    }
+}
