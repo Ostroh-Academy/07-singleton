f444529f5d7f1b24a5a3774a5ae90809f4a7c479
Nastya <92024271+havrylchyk@users.noreply.github.com>
2024-04-22 21:52:06 +0300

    Part 2

---
+++ Lab7_2_Singleton_Patterns/Lab7_2_Singleton_Patterns/Program.cs
@@ -0,0 +1,7 @@
+﻿ConfigurationManager configManager = ConfigurationManager.Instance;
+
+string databaseConnectionString = configManager.GetConfigValue("DatabaseConnectionString");
+Console.WriteLine("Database Connection String: " + databaseConnectionString);
+
+string apiKey = configManager.GetConfigValue("APIKey");
+Console.WriteLine("API Key: " + apiKey);
