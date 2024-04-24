d944326c39786e194dec0011fdc29109cd1bf56f
nesterchuk.iraida03@gmail.com <nesterchuk.iraida03@gmail.com>
2024-04-24 19:42:29 +0300

    Виконала завдання лабораторної 7

---
+++ lab 7/lab 7/LogMessage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_7
+{
+    public class LogMessage
+    {
+        public string Message { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public LogMessage(string message)
+        {
+            Message = message;
+            Timestamp = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return $"{Timestamp} - {Message}";
+        }
+    }
+}
---
+++ lab 7/lab 7/LoggerClient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_7
+{
+    public class LoggerClient
+    {
+        private static LoggerClient _instance;
+        private List<LogMessage> _logMessages;
+
+        private LoggerClient()
+        {
+            _logMessages = new List<LogMessage>();
+        }
+
+        public static LoggerClient GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new LoggerClient();
+            }
+            return _instance;
+        }
+
+        public void Log(string message)
+        {
+            var logMessage = new LogMessage(message);
+            _logMessages.Add(logMessage);
+        }
+
+        public void PrintLog()
+        {
+            Console.WriteLine("Logged messages:");
+            foreach (var logMessage in _logMessages)
+            {
+                Console.WriteLine(logMessage);
+            }
+        }
+    }
+}
