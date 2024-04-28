4ecc4d62ed097abd72b0227500031f646418cc52
OleksiiKliain <alexkliain35@gmail.com>
2024-04-28 21:42:22 +0300

    done

---
+++ TemplateSingelton/TemplateSingelton/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace TemplateSingelton
+{
+
+        public class Singleton
+        {
+            private static Singleton instance;
+
+            private Singleton() { }
+
+            public static Singleton GetInstance()
+            {
+                if (instance == null)
+                    instance = new Singleton();
+                return instance;
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Singleton singleton1 = Singleton.GetInstance();
+                Singleton singleton2 = Singleton.GetInstance();
+
+                if (singleton1 == singleton2)
+                    Console.WriteLine("Обєкти схожі");
+                else
+                    Console.WriteLine("Обєкти різні");
+
+                Console.ReadLine();
+            }
+        }
+    }
+
