6cf30383d45ffd647aa62c0abb978ffda9656fc7
Юлія Денисюк <yuliia.denysiuk@oa.edu.ua>
2024-04-28 22:43:07 +0300

    Лабораторна робота №7

---
+++ Denysiuk_Pattern_Singleton/ConfigurationData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Denysiuk_Pattern_Singleton
+{
+    public class ConfigurationData
+    {
+        public string DatabaseConnectionString { get; set; }
+        public int Port { get; set; }
+        public string ApiKey { get; set; }
+
+        // Other properties for configuration data...
+    }
+}
