c8eb9b52f55a639633c729c0001ac294bb77354a
Iryna Krush <145108812+Ira-name@users.noreply.github.com>
2024-04-29 00:04:14 +0300

    Add files via upload

---
+++ Food.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_7_P
+{
+    using System;
+
+    public class FoodLoggerSingleton
+    {
+        private static FoodLoggerSingleton _instance;
+        private int _foodCount = 0;
+        private FoodLoggerSingleton() { }
+        public static FoodLoggerSingleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new FoodLoggerSingleton();
+            }
+            return _instance;
+        }
+        public void LogFood(string foodName)
+        {
+            Console.WriteLine(_foodCount + ": " + foodName);
+            _foodCount++;
+        }
+    }
+    public class Chef
+    {
+        public void Cook(string foodName)
+        {
+            FoodLoggerSingleton logger = FoodLoggerSingleton.GetInstance();
+            logger.LogFood("Cooking " + foodName + "...");
+            logger.LogFood("Finished cooking " + foodName + ".");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chef chef = new Chef();
+
+            chef.Cook("Pasta");
+            chef.Cook("Pizza");
+            chef.Cook("Salad");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnlineShopping
+{
+    public class ShoppingCartSingleton
+    {
+        private static ShoppingCartSingleton _instance;
+        private List<string> _items;
+
+        private ShoppingCartSingleton()
+        {
+            _items = new List<string>();
+        }
+        public static ShoppingCartSingleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new ShoppingCartSingleton();
+            }
+            return _instance;
+        }
+        public void AddItem(string item)
+        {
+            _items.Add(item);
+            Console.WriteLine("Додано товар '{0}' до кошика.", item);
+        }
+        public void RemoveItem(string item)
+        {
+            if (_items.Remove(item))
+            {
+                Console.WriteLine("Видалено товар '{0}' з кошика.", item);
+            }
+            else
+            {
+                Console.WriteLine("Товар '{0}' не знайдено у кошику.", item);
+            }
+        }
+        public void DisplayItems()
+        {
+            Console.WriteLine("Товари у кошику:");
+            foreach (var item in _items)
+            {
+                Console.WriteLine("- " + item);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShoppingCartSingleton cart = ShoppingCartSingleton.GetInstance();
+            cart.AddItem("Футболка");
+            cart.AddItem("Штани");
+            cart.AddItem("Кросівки");
+            cart.DisplayItems();
+            ShoppingCartSingleton anotherCart = ShoppingCartSingleton.GetInstance();
+            Console.WriteLine("Обидва екземпляри вказують на один і той же кошик: " + (cart == anotherCart));
+            cart.RemoveItem("Штани");
+            cart.DisplayItems();
+        }
+    }
+}
