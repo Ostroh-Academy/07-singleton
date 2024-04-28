080622a4c1c18497574f7cfe4a9cb6608c6712f8
Andrew <shikigamori@gmail.com>
2024-04-28 13:22:19 +0300

    Завершив лабу №7 (8 варіант)

---
+++ Laba7/Laba7/Program.cs
@@ -0,0 +1,13 @@
+﻿using Laba7;
+
+TrainReservationSystem system = TrainReservationSystem.Instance;
+TrainReservationSystem system2 = TrainReservationSystem.Instance;
+
+system.ReserveSeat(10);
+system2.ReserveSeat(20);
+
+system.ReserveSeat(20);
+system.ReserveSeat(10);
+
+Console.WriteLine($"Чи посилаються обидва об'єкти {nameof(system)} та {nameof(system2)} на один екземпляр класу " +
+    $"{nameof(TrainReservationSystem)}: {system == system2}");
---
+++ Laba7/Laba7/TrainReservationSystem.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba7
+{
+    internal class TrainReservationSystem
+    {
+        private static TrainReservationSystem _instance;
+
+        private bool[] seats = new bool[100];
+
+        private TrainReservationSystem() { }
+
+        public static TrainReservationSystem Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new TrainReservationSystem();
+                }
+
+                return _instance;
+            }
+        }
+
+        public bool ReserveSeat(int seatNumber)
+        {
+            if (seatNumber < 0 || seatNumber >= seats.Length)
+            {
+                Console.WriteLine("Неправильний номер місця.");
+                return false;
+            }
+
+            if (seats[seatNumber] == true)
+            {
+                Console.WriteLine($"Місце {seatNumber} вже заброньоване.");
+                return false;
+            }
+
+            seats[seatNumber] = true;
+            Console.WriteLine($"Місце {seatNumber} успішно заброньоване.");
+            return true;
+        }
+    }
+}
