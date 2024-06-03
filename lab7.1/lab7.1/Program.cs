57f2dd65e5b8dc728018134300925d9d92a6cd9a
haletik <92076714+haletik@users.noreply.github.com>
2024-06-03 18:25:10 +0300

    Add files via upload

---
+++ lab7.1/lab7.1/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Singleton
+{
+    private static Singleton instance;
+
+    private Singleton() { }
+
+    public static Singleton Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+    }
+
+    public void SomeMethod()
+    {
+        // Додатковий код
+    }
+}
---
+++ lab7.2/lab7.2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+
+public class Logger
+{
+    private static Logger instance;
+    private static readonly object lockObject = new object();
+    private string logFilePath = "log.txt";
+
+    private Logger() { }
+
+    public static Logger Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                lock (lockObject)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Logger();
+                    }
+                }
+            }
+            return instance;
+        }
+    }
+
+    public void Log(string message)
+    {
+        try
+        {
+            using (StreamWriter sw = File.AppendText(logFilePath))
+            {
+                sw.WriteLine($"{DateTime.Now}: {message}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error logging message: {ex.Message}");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Logger.Instance.Log("Application started");
+        // Виконання дій
+        Logger.Instance.Log("Some action occurred");
+        // Додаткові дії
+        Logger.Instance.Log("Application stopped");
+    }
+}
