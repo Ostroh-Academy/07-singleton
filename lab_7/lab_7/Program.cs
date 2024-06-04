a2b28bf6b4013e460e0db289daadf325383b556a
ViktoriaDenyshchuk <92024304+ViktoriaDenyshchuk@users.noreply.github.com>
2024-06-04 20:00:59 +0200

    Add files via upload

---
+++ lab_7/lab_7/Program.cs
@@ -0,0 +1,13 @@
+﻿using lab_7;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Logger logger = Logger.GetInstance();
+
+        logger.Log("Application started.");
+        logger.Log("Something happened.");
+        logger.Log("Application stopped.");
+    }
+}
