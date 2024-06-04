282369ce8618e6344ddef394bdae93ba271a8319
ViktoriaDenyshchuk <92024304+ViktoriaDenyshchuk@users.noreply.github.com>
2024-06-04 19:59:47 +0200

    Add files via upload

---
+++ example_7/example_7/Program.cs
@@ -0,0 +1,22 @@
+﻿using example_7;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Отримання єдиного екземпляру класу Settings
+        Settings settings = Settings.GetInstance();
+
+        // Перевірка налаштувань
+        Console.WriteLine("Example Setting: " + settings.ExampleSetting);
+
+        // Можна змінити налаштування через будь-який екземпляр класу Settings
+        settings.ExampleSetting = "New Value";
+
+        // Отримання іншого екземпляру класу Settings
+        Settings anotherSettings = Settings.GetInstance();
+
+        // Перевірка, чи збереглися змінені налаштування
+        Console.WriteLine("Example Setting after modification: " + anotherSettings.ExampleSetting);
+    }
+}
---
+++ example_7/example_7/Settings.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace example_7
+{
+    public class Settings
+    {
+        private static Settings instance;
+        private static object lockObject = new object();
+
+        // Приклад налаштування
+        public string ExampleSetting { get; set; }
+
+        // Приватний конструктор
+        private Settings()
+        {
+            // Ініціалізація прикладових налаштувань
+            ExampleSetting = "Default Value";
+        }
+
+        // Статичний метод для отримання єдиного екземпляру класу
+        public static Settings GetInstance()
+        {
+            // Перевірка, чи екземпляр вже існує
+            if (instance == null)
+            {
+                // Забезпечення потокобезпеки за допомогою lock
+                lock (lockObject)
+                {
+                    // Перевірка ще раз, щоб уникнути створення дублюючих екземплярів у многопотоковому середовищі
+                    if (instance == null)
+                    {
+                        instance = new Settings();
+                    }
+                }
+            }
+            return instance;
+        }
+    }
+}
+
