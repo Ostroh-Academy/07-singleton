117eec82216193abe244356e6237ae7b42553c6b
Valera Sloza <valera.sloza@gmail.com>
2024-06-07 14:27:06 +0300

    Done

---
+++ Laba7/Laba7/Program.cs
@@ -0,0 +1,30 @@
+﻿using Laba7;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var tasks = new Task[]
+        {
+                Task.Run(() => PerformUserAction("User1", "Login")),
+                Task.Run(() => PerformUserAction("User2", "Logout")),
+                Task.Run(() => PerformUserAction("User3", "View Page")),
+                Task.Run(() => PerformUserAction("User4", "Edit Profile")),
+                Task.Run(() => PerformUserAction("User5", "Send Message"))
+        };
+
+        await Task.WhenAll(tasks);
+
+        Console.WriteLine("All actions performed:");
+        foreach (var action in SynchronizationService.Instance.GetActions())
+        {
+            Console.WriteLine(action);
+        }
+    }
+
+    static void PerformUserAction(string user, string action)
+    {
+        var syncService = SynchronizationService.Instance;
+        syncService.PerformAction(user, action);
+    }
+}
---
+++ Laba7/Laba7/SynchronizationService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba7
+{
+    public class SynchronizationService : ISynchronizationService
+    {
+        private static readonly object _lock = new object();
+        private static SynchronizationService _instance;
+        private readonly List<string> _actions;
+
+        private SynchronizationService()
+        {
+            _actions = new List<string>();
+        }
+
+        public static SynchronizationService Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new SynchronizationService();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        public void PerformAction(string user, string action)
+        {
+            lock (_lock)
+            {
+                _actions.Add($"{user} performed {action}");
+                Console.WriteLine($"{user} performed {action}");
+            }
+        }
+
+        public IEnumerable<string> GetActions()
+        {
+            lock (_lock)
+            {
+                return _actions.ToArray();
+            }
+        }
+    }
+}
